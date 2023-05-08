2f3b302e19bb3728f7822e0076bb01825dd5b2fc
Project-Ice-Man <124945757+Project-Ice-Man@users.noreply.github.com>
2023-05-08 14:08:19 -0700

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
 
-import pandas as pd, time
+import pandas as pd, time, pickle, json
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 app = Flask(__name__)
 
@@ -10,6 +9,9 @@ app = Flask(__name__)
 def predict():
     try:
         start_time = time.time()
+
+        # read
+        read_path = '/Volumes/Windows/Win/Code/RiskThinking.AI'
 
         # query string | parameters
         args = request.args
@@ -19,26 +21,16 @@ def predict():
         vol_moving_avg        = args.get('vol_moving_avg')
         adj_close_rolling_med = args.get('adj_close_rolling_med')
 
-        # train files saved during pipeline execution in folder /Problem 3/
-        X_train = pd.read_pickle(f'/Volumes/Windows/Win/Code/RiskThinking.AI/Problem 3/{Symbol}/X_train.pkl')
-        y_train = pd.read_pickle(f'/Volumes/Windows/Win/Code/RiskThinking.AI/Problem 3/{Symbol}/y_train.pkl')
+        # blanks
+        error, model = {}, RandomForestRegressor(n_estimators=100, random_state=42)
 
-        # Create a RandomForestRegressor model
-        model = RandomForestRegressor(n_estimators=100, random_state=42)
+        # loading the trained model
+        with open(f'{read_path}/Problem 3/{Symbol}/model.pkl', 'rb') as f:
+            model = pickle.load(f)
 
-        # Train the model
-        model.fit(X_train, y_train)
-
-        # test files saved during pipeline execution in folder /Problem 3/
-        X_test = pd.read_pickle(f'/Volumes/Windows/Win/Code/RiskThinking.AI/Problem 3/{Symbol}/X_test.pkl')
-        y_test = pd.read_pickle(f'/Volumes/Windows/Win/Code/RiskThinking.AI/Problem 3/{Symbol}/y_test.pkl')
-
-        # Make predictions on test data
-        y_pred = model.predict(X_test)
-
-        # Calculate the Mean Absolute Error and Mean Squared Error
-        mae = mean_absolute_error(y_test, y_pred)
-        mse = mean_squared_error(y_test, y_pred)
+        # loading the error info
+        with open(f'{read_path}/Problem 3/{Symbol}/error.json', 'r') as f:
+            error = json.load(f)
 
         # predicting
         pred = ( model.predict(
@@ -51,11 +43,12 @@ def predict():
                             f'<td class=right>        vol_moving_avg  <td> {float(vol_moving_avg):,.2f}',
                             f'<td class=right> adj_close_rolling_med  <td> {float(adj_close_rolling_med):,.6f}',
                             f'<td class=right>      Predicted Volume  <th> {int(pred[0]):,}',
-                            f'<td class=right>   Mean Absolute Error  <td> {mae:,.2f}',
-                            f'<td class=right>   Mean  Squared Error  <td> {mse:,.2f}',
+                            f'<td class=right>   Mean Absolute Error  <td> {float(error["mean_absolute_error"]):,.2f}',
+                            f'<td class=right>   Mean  Squared Error  <td> {float(error["mean_squared_error"]):,.2f}',
                             f'<td class=right>        Execution Time  <td> {round(time.time() - start_time, 2):.2f} sec' ]) +
 
-            '\n</table>\n<style> td, th {font: 9pt Verdana} th {text-align: left; background-color: lemonchiffon} .right {text-align: right; padding-right: 10} </style>')
+            '\n</table>\n<style> td, th {font: 9pt Verdana} th {text-align: left; background-color: lemonchiffon} .right {text-align: right; padding-right: 10} </style>'
+        )
 
     # in case something bad happened
     except Exception as x:
