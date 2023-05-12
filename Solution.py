67ffff876b5a57d79d0fdc165ce8343e8e8f2ba7
Project-Ice-Man <124945757+Project-Ice-Man@users.noreply.github.com>
2023-05-11 21:53:13 -0700

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -100,7 +100,7 @@ for svm in symbols_valid_meta.collect():
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         # Create a RandomForestRegressor model
-        model = RandomForestRegressor(n_estimators=100, random_state=42)
+        model = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=42)
 
         # Train the model
         model.fit(X_train, y_train)
